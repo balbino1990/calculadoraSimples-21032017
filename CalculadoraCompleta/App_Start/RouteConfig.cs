5ab14c231df434f3e130ad485ce15d3f45efe78f
balbino1990 <balbino.pinto@yahoo.com>
2017-03-21 13:07:00 +0000

    versão inicial do interface do calculadora

--- CalculadoraCompleta/App_Start/RouteConfig.cs
+++ CalculadoraCompleta/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CalculadoraCompleta
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Calculadora", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CalculadoraCompleta/Controllers/CalculadoraController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraCompleta.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Calculadora
+        [HttpPost]
+        public ActionResult Index(string bt)
+        {
+            return View();
+        }
+    }
+}
